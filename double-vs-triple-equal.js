7b0f73bbaa44be75ce6b9f160e0b5ad3b8a37da8
mahmud imran <mahmudimran203@gmail.com>
2021-02-02 19:49:55 +0600

    double vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,15 @@
+const first = 0;
+const second = false;
+if(first === second){
+    console.log('hey Coder Code true');
+}
+else{
+    console.log('hey Coder code is False');
+}
+
+if ('2'=== 2){
+    console.log('inside if')
+}
+else{
+    console.log('inside else')
+}
