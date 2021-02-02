0aec76fb2f72a901ae308baf20dbe2eb336126a5
mahmud imran <mahmudimran203@gmail.com>
2021-02-02 21:41:24 +0600

    clouser

---
+++ scope.js
@@ -0,0 +1,20 @@
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    console.log(result);
+    if (result > 9) {
+        var mood = "happy";
+         mood = "fidhy";
+         mood = " funky";
+         mood = "crinky";
+        console.log(mood);
+    }
+    
+   const day = "friday"
+   console.log(day);
+    
+    return result;
+}
+const output = sum(3,8);
+// console.log(bonus);
+// console.log(output);
