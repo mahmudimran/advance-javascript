6bf03c640c2a71aeb2456acd5e5e6ef8901f21a5
mahmud imran <mahmudimran203@gmail.com>
2021-02-02 19:27:49 +0600

    null vs undefined clrears intervew

---
+++ undefined.js
@@ -0,0 +1,19 @@
+let pakhiEkhonPakhJalaise;
+
+function add(num1, num2){
+    console.log(num1+num2);
+    return;
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const premik = {name : 'kulsom', phone: 12456};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [21, 22, 23];
+console.log(ages[25]);
