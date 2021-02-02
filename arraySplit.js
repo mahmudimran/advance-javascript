709949bf65ed7a4d3c37a0d9d73fdad5fb1ab876
mahmud imran <mahmudimran203@gmail.com>
2021-02-02 21:57:36 +0600

    array slice splice

---
+++ arraySplit.js
@@ -0,0 +1,7 @@
+const nums = [1, 2, 3, 4, 5, 6,7, 8];
+// const part = nums.slice(2, 5);
+const removed = nums.splice(2, 5, 77,55);
+// console.log(nums);
+// console.log(removed);
+const together = nums.join(" Ami ")
+console.log(together);
